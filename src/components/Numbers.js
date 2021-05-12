b734b24b975b3df142eb9ac0304314926b828a74
GhostArena <garielomane@outlook.com>
2021-05-12 16:46:33 +0000

    Coinbase Clone first update

---
+++ src/components/Numbers.js
@@ -0,0 +1,22 @@
+const Numbers = () => {
+    return (
+        <div className="container-fluid contMain colText">
+            <div className="row rowAlign">
+                <div className="col-md-4"> 
+                    <h1>$335B</h1>
+                    <p className="text-center">Quarterly volume traded</p> 
+                </div>
+                <div className="col-md-4"> 
+                    <h1>100+</h1>
+                    <p className="text-center">Countries supported</p> 
+                </div>
+                <div className="col-md-4"> 
+                    <h1>56+M</h1>
+                    <p className="text-center">Verified users</p> 
+                </div>
+            </div>        
+        </div>
+    )
+};
+
+export default Numbers;
---
+++ src/components/StartEarn.js
@@ -0,0 +1,32 @@
+import coinimg from '../images/coinimg.jpg';
+
+const StartEarn = () => {
+    return (
+      <div className="container-fluid earnBox">
+        <div className="row">
+          <div className="col-md-6">
+              <div className="container innerEarnBox">
+                
+                    <h2 className="">Earn up to $25 worth of crypto</h2>
+                    <p className="earnBox-p text-left">
+                        Discover how specific cryptocurrencies work — and get a bit of
+                        each crypto to try out for yourself.
+                    </p>
+                    <a color="slate" weight="inherit" className="" href="/earn">
+                    <button type="primary" className="btn btn-primary">
+                        <span className="">Start earning</span>
+                    </button>
+                    </a>
+                
+              </div>
+           
+          </div>
+          <div className="col-md-6">
+            <img src={coinimg} alt="alt" />
+          </div>
+        </div>
+      </div>
+    );
+};
+
+export default StartEarn;
