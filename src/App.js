7b9679b5ebfe1c0ec60fe572e50a4cd05746ee29
naedeezy <naedeezy@gmail.com>
2021-05-16 22:34:56 +0000

    first push

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import Numbers from "./components/Numbers";
 import StartEarn from "./components/StartEarn";
 import Started from "./components/Started";
+import Phone from "./components/Phone";
 
 function App() {
   return (
     <div className="App">
       
+      <Phone/>
       <Numbers/>
       <Started/>
       <StartEarn/>
---
+++ src/components/Phone.js
@@ -0,0 +1,16 @@
+import {ReactComponent as BitCoinIcon } from "../Resources/bitcoinlogo.svg";
+
+const Phone = () => {
+    return (
+        <div className="grid-cols-2 max-w-7xl">
+            <div className="flex">
+                <div className="flex">
+                    <BitCoinIcon />
+                </div>
+            </div>
+        
+        </div>
+    );
+};
+
+export default Phone;
